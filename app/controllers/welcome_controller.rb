90eb1d3979e906d0453da736250ef38dcb3708fb
Taehun Kim <kimx2815@umn.edu>
2018-04-26 18:05:08 -0700

    rand gen board

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,17 @@
 class WelcomeController < ApplicationController
-  def index
 
-  	@r1, @r2, @r3, @r4 = "ABCD", "DEFG", "ABCD", "DEFG"
+  def index
+  	@r1, @r2, @r3, @r4 = board_gen() #{}"ABCD", "DEFG", "ABCD", "DEFG"
   	@props = { name: "Stranger", r1: @r1, r2: @r2, r3: @r3, r4: @r4 }
 
-
-
   end
+
+  private 
+  	def board_gen
+  		return [rand_chars(),rand_chars(),rand_chars(),rand_chars()]
+  	end
+
+  	def rand_chars
+  		(0...4).map { (65 + rand(26)).chr }.join
+  	end
 end
