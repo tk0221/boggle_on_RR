259bd49adb80932b6d79e9c5262fb27b70ea9edc
Taehun Kim <kimx2815@umn.edu>
2018-04-29 05:46:18 -0700

    set up test case

---
+++ spec/game_data_spec.rb
@@ -0,0 +1,11 @@
+require "GameData"
+
+describe GameData do
+  game_data = GameData.new
+  input_string = "GameData Test"
+
+  describe "#GameData load" do
+    it "returns game data correctly" do
+    end
+  end
+end
