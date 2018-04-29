5ec859c10f4974ce47ca7c4d0da84bd84771b6be
Taehun Kim <kimx2815@umn.edu>
2018-04-28 22:21:28 -0700

    root -> welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-	get 'welcome/index'
+	
+	root :to => "welcome#index"
+	# get 'welcome/index'
 
 	resources :welcome
-	namespace :api do
-		namespace :v1 do
-			resources :gamedatas
-		end
-	end
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
